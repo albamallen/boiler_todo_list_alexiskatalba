827bf340be85a45f2a107e27081d45cf7b0b03d6
albamallen <albamallenm@gmail.com>
2024-02-14 18:37:06 +0100

    removealba

--- app/main.js
+++ app/main.js
@@ -1,3 +1,20 @@
+// import { tareas } from '../assets/data/tareas.js';
+
+// window.onload = () => {
+//     let lista = document.querySelector("#lista");
+
+//     tareas.forEach((tarea) => {
+//         let elem = `
+//             <li class="lista_elemento" id="${tarea.id}">
+//                 <p>${tarea.titulo}</p>
+//                 <button class="btn_cerrar">X</button>
+//             </li>
+//         `;
+//         console.log(elem)
+//         lista.innerHTML+=elem;
+//     })
+// }
+
 import { tareas } from '../assets/data/tareas.js';
 
 window.onload = () => {
@@ -10,8 +27,17 @@ window.onload = () => {
                 <button class="btn_cerrar">X</button>
             </li>
         `;
-        console.log(elem)
         lista.innerHTML+=elem;
-    })
-}
+    });
 
+    // Agregar evento click a los botones de cierre
+    let botonesCerrar = document.querySelectorAll(".btn_cerrar");
+    botonesCerrar.forEach((boton) => {
+        boton.addEventListener("click", () => {
+            // Obtener el elemento padre del botón (que es el <li>)
+            let elementoPadre = boton.parentNode;
+            // Eliminar el elemento de la lista
+            lista.removeChild(elementoPadre);
+        });
+    });
+}
